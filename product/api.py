fa6422de3a57e5fe7d69165a444debaa598b6270
Zuzanna Misztal <zuzanna.misztal@kaseya.com>
2022-12-30 14:22:08 +0100

    implement all

--- product/api.py
+++ product/api.py
@@ -1,19 +1,37 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, request, render_template
+from redis import Redis
 
-app = Flask(__name__)
-api = Api(app)
+app = Flask(__name__, template_folder="templates")
 
-class Product(Resource):
-    def get(self):
-        return {
-            'products': ['Ice cream',
-                        'Chocolate',
-                        'Fruit',
-                        'Vodka']
-        }
+redis = Redis(host='redis', port=6379)
 
-api.add_resource(Product, '/')
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/write')
+def write_form():
+    return render_template('write.html')
+
+@app.route('/write', methods=['POST'])
+def write_post():
+    first = request.form['first']
+    last = request.form['last']
+    redis.set(first, last)
+    return render_template('writereturn.html', data=(first,last))
+
+@app.route('/get')
+def get():
+    return render_template('getform.html')
+
+@app.route('/get', methods=['GET', 'POST'])
+def get_post():
+    first = request.form['first']
+    last = redis.get(first)
+    if last:
+        last = last.decode()
+    return render_template('getreturn.html', data=(first,last))
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0')
